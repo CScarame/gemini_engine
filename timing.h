46e98eeeed5715706737b2b45259994dca104a96
BScaramella18 <bvs7@cornell.edu>
2016-06-29 01:41:50 -0400

    Adding timing.h

---
+++ timing.h
@@ -0,0 +1,12 @@
+// Header file for timing inputs
+#ifndef _TIMING_H
+#define _TIMING_H
+
+#include <time.h>
+
+
+// Get an amount of nanoseconds representing the
+// current time
+void get_time(long long* ts);
+
+#endif
