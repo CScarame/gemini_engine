1a2fb07e8b2aca696abd9e54dccff067a590a7b7
BScaramella18 <bvs7@cornell.edu>
2016-07-01 04:03:08 -0400

    Commented keyboard.h

--- CONSOLE_VERSION/keyboard.h
+++ CONSOLE_VERSION/keyboard.h
@@ -16,13 +16,14 @@
 #include <linux/kd.h>
 #include <linux/keyboard.h>
 
+// Enum for key inputs
 typedef enum
 {
   k_up,
   k_left,
   k_down,
   k_right,
-  k_rump,
+  k_jump,
   k_stance,
   k_a,
   k_b,
@@ -33,16 +34,27 @@ typedef enum
   num_keys
 } keyflag_t;
 
+// Read key presses from the buffer and update keys
 void tick();
 
+// Updates keys variable
 void update_keys(short keycode, char down);
 
+// Return value has a bit for each key above, 1 is down, 0 up
+// LSB is k_up.
 int get_keys();
 
+// Returns 1 if k is down
 char check_key(keyflag_t k);
 
+// Called to begin keyboard input.
+// Enables medium raw input (keycodes)
+// Disables console input.
+// Non-canonical inputs (No enter is needed, every press sends a code
+// Disables repeat
 void init();
 
+// Returns keyboard to former settings 
 void cleanup();
 
 #endif // _KEYBOARD_H
