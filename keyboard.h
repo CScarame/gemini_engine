93e9abb3161e4b836acaf88f1327baab68bc3cb1
BScaramella18 <bvs7@cornell.edu>
2016-07-01 04:19:26 -0400

    Minor comments.

--- keyboard.h
+++ keyboard.h
@@ -7,14 +7,14 @@
 #define DEBUG 0
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <linux/kd.h>
-#include <linux/keyboard.h>
+#include <stdio.h>   // For printf
+//#include <stdlib.h>
+//#include <unistd.h>
+#include <termios.h> // For various keyboard input config
+#include <fcntl.h>  // For file protections
+//#include <sys/ioctl.h>
+#include <linux/kd.h> // For kb mode and other keyboard config
+//#include <linux/keyboard.h>
 
 // Enum for key inputs
 typedef enum
@@ -37,7 +37,7 @@ typedef enum
 // Read key presses from the buffer and update keys
 void tick();
 
-// Updates keys variable
+// Updates keys variable. Down is a boolean.
 void update_keys(short keycode, char down);
 
 // Return value has a bit for each key above, 1 is down, 0 up
