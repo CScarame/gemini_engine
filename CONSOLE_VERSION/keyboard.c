8a3723cd1c0da4c4c0b290e0bcd0a4d3a6de714d
BScaramella18 <bvs7@cornell.edu>
2016-07-01 21:27:38 -0400

    remap?

--- CONSOLE_VERSION/keyboard.c
+++ CONSOLE_VERSION/keyboard.c
@@ -1,17 +1,17 @@
  // Header file specifically for console/small_screen
 //#define DEBUG 1
-#include "keyboard.h"
+#include "../keyboard.h"
 
-int fd = 0;
-int oldkbmode = K_RAW;
-struct termios orig_kb;
-struct termios new_kb;
+int fd = 0; // The file descriptor for the text input
+int oldkbmode = K_RAW;  // Used to hold the old keyboard mode, to reset
+struct termios orig_kb; // Old termios keyboard data, to reset
+struct termios new_kb;  // New termios keyboard data for use in this program
 
-unsigned char buf;
+char active = 0;  // Is 1 after init has been called and before cleanup
 
-char active = 0;
+int keys = 0;  // Variable that holds key status
 
-int keys = 0;
+int remap = -1; // If this isn't -1, a key is being remapped
 
 unsigned char keycodes[] = {
   17,
@@ -28,22 +28,7 @@ unsigned char keycodes[] = {
   1
 };
 
-// If init has been called, then finds the status of keys
-void tick()
-{
-  if(!active) return;
-
-  if(DEBUG >= 2) printf("tick keyboard");
-
-  char down;
-  unsigned char keycode;
-  while(read(fd,&buf,1) >= 0) {
-    down = !(buf & 0x80); // bit is 0 for down, 1 for up
-    keycode = buf & 0x7F;    // The rest is put into keycode
-    update_keys(keycode,down);
-  }
-}
-
+// Updates the status of the keys variable.
 void update_keys(short keycode, char down)
 {
   if(DEBUG >= 1) {
@@ -55,15 +40,42 @@ void update_keys(short keycode, char down)
     }
   }
 
-  int i;
-  for(i=0;i<num_keys;i++){
-    if(keycode == keycodes[i]){
-      if(down){
-        keys |= (1<<i); // Mask the bit on
-      } else{
-        keys &= ~(1<<i);// Mask the bit off
+  if(remap == -1){
+    int i;
+    for(i=0;i<num_keys;i++){
+      if(keycode == keycodes[i]){
+        if(down){
+          keys |= (1<<i); // Mask the bit on
+        } else{
+          keys &= ~(1<<i);// Mask the bit off
+        }
       }
     }
+  }else {
+    if(down){
+      keycodes[remap] = keycode;
+      remap = -1;
+    }
+  }
+}
+
+
+// If init has been called, then finds the status of keys
+void tick()
+{
+  if(!active) return;
+
+  if(DEBUG >= 2) printf("tick keyboard");
+
+  char down; // Used to hold whether the key was moved to up or down
+  unsigned char keycode; // Used to hold the keycode of the presed key
+
+  unsigned char buf;  // Characters are read one at a time to this buffer.
+
+  while(read(fd,&buf,1) >= 0) {
+    down = !(buf & 0x80); // bit is 0 for down, 1 for up
+    keycode = buf & 0x7F;    // The rest is put into keycode
+    update_keys(keycode,down);
   }
 }
 
@@ -73,6 +85,19 @@ int get_keys(){
 
 char test_key(keyflag_t k){
   char result = (keys & (1<<k))?1:0;
+}
+
+// After this function is called, the next key to be pressed
+// Becomes the mapping for the parameter keyflag
+int remap_key(keyflag_t k){
+  remap = (int)k;
+  int temp = remap;
+
+  // The following is used to freeze everything while the key is being remapped
+  while(remap != -1){
+    tick();
+  }
+  return (int)keycodes[temp];
 }
 
 void init()
@@ -117,7 +142,10 @@ void cleanup()
 int main() // For Testing only
 {
   init();
-  keys = 0;
+
+  remap_key(k_up);
+  printf("%d\n",keycodes[0]);
+
   int i;
   while(!(test_key(k_esc))){
     tick();
