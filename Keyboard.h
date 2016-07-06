603b32835aa33bd596d8a6ed819206e69f8a47a6
Brian <Brian@BSCARAMELLA-PC>
2016-07-05 23:10:12 -0700

    Added base abstract class for Keyboard

---
+++ Keyboard.h
@@ -0,0 +1,43 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "util.h"
+
+// Enum for key inputs
+typedef enum
+{
+	k_up,
+	k_left,
+	k_down,
+	k_right,
+	k_jump,
+	k_stance,
+	k_a,
+	k_b,
+	k_x,
+	k_y,
+	k_enter,
+	k_esc,
+	num_keys
+} keyflag_t;
+
+class Keyboard
+{
+public:
+	Keyboard() {};
+	
+	~Keyboard() {};
+
+	virtual void update() = 0;
+	// Updates the internal key variables to reflect the status of the keyboard
+
+	virtual int get_keys() = 0;
+	// Returns an int with bits for each key
+
+	virtual char check_key(keyflag_t) = 0;
+	// Returns boolean "if key is down"
+};
+
+
+
+#endif
